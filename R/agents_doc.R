80a3aaca027a78f7ec0b5f5ffa5059e9c5243002
jrhammond <jesse.hammond1@gmail.com>
2015-11-24 22:01:49 -0800

    Re-adding data files.

---
+++ R/agents_doc.R
@@ -0,0 +1,14 @@
+#'
+#' ICEWS CAMEO actor codes
+#'
+#' Merge table to convert entries to CAMEO format using conversion tables created
+#'    by Phil Schrodt (https://github.com/philip-schrodt/text_to_CAMEO)
+#'
+#'  @docType data
+#'
+#'  @usage data(agents)
+#'
+#'  @keywords datasets
+#'
+#'
+"agents"
---
+++ R/states_doc.R
@@ -0,0 +1,14 @@
+#'
+#' ICEWS CAMEO actor codes
+#'
+#' Merge table to convert states to CAMEO format using conversion tables created
+#'    by Phil Schrodt (https://github.com/philip-schrodt/text_to_CAMEO)
+#'
+#'  @docType data
+#'
+#'  @usage data(states)
+#'
+#'  @keywords datasets
+#'
+#'
+"states"
